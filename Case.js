4edafb920479fb222169f020c29fc8b38a4bdb0d
Rezwanulrimel <rezwanulrimels@gmail.com>
2026-04-04 01:52:38 +0600

    Added Case

---
+++ Case.js
@@ -0,0 +1,26 @@
+const Versity = "Raj UK Uttara model School";
+console.log(Versity);
+
+// toUpperCase
+console.log(Versity.toUpperCase());
+// Tolowercase
+console.log(Versity.toLowerCase());
+
+const subjcets = "DiscreteMathematics";
+const book = "Mathematics";
+
+if (subjcets.toLowerCase() === book.toLowerCase()) {
+  console.log("ami ongke onek valo");
+} else {
+  console.log("ami math e onek kharap ");
+}
+
+// white space
+const rimel = "js";
+const rezwan = "   js";
+
+if (rimel.trim() === rezwan.trim()) {
+  console.log("tumi js valo kore sikho");
+} else {
+  console.log("age concept clear koro");
+}
